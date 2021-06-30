1ae6528cb9c3ac484296160e3fa5b3cca6ee99de
Paul Goux <paul.goux@wanadoo.fr>
2021-06-30 18:55:06 +0200

    WIP trying to create controller with included bundle

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class TaskController extends AbstractController
+{
+    public function index(TaskRepository $taskRepository): Response
+    {
+        return new JsonResponse($taskRepository->findAll());
+    }
+
+    public function new(Request $request): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return new JsonResponse(null, 201);
+        }
+
+        return new JsonResponse(null, 400);
+    }
+
+    public function show(Task $task): Response
+    {
+        return new JsonResponse($task);
+    }
+
+    public function edit(Request $request, Task $task): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('task_index');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'task' => $task,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    public function delete(Request $request, Task $task): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$task->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($task);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('task_index');
+    }
+}
